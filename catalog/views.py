b10867e2d17c0041adf7a0f6a6ed55971db58412
Sarason Andrei <sarason@rambler.ru>
2025-06-21 13:58:00 +0300

    completed the tasks of lesson 24

--- catalog/views.py
+++ catalog/views.py
@@ -1,9 +1,21 @@
 # catalog/views.py
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 from django.shortcuts import render
 
+
 def home(request):
-    return render(request, 'catalog/home.html')  # Отображение шаблона home.html
+    products = Product.objects.all()
+    return render(request, 'catalog/home.html', {'products': products})  # Отображение шаблона home.html
+
 
 def contact(request):
     return render(request, 'catalog/contacts.html')  # Отображение шаблона contact.html
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'catalog/product_detail.html', {'product': product})
+
+
